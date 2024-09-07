1ab640b36578e9e808c390046813c12c6e4c3795
umesh-2045 <umeshtejas2004@gmail.com>
2024-09-07 19:34:31 +0000

    conversion_of_datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// check the datatype
+
+/*
+"33" = 33
+"33abc" = nan
+true = 1; false= 0
+" " = false
+"hitesh" = true
+*/
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
