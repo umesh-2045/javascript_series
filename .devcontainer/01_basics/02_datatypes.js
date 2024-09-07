83c9b9e1235ef9356c26922a0865d841207c93bc
umesh-2045 <umeshtejas2004@gmail.com>
2024-09-07 19:17:08 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert ( 3+3) we are using nodejs not browser
+// code readability should be high
+
+let name = "umesh"// string
+let age = 20// integer
+let isloggedin = false// bolean 
+
+//number => 2 to power 53
+//bogint
+// string => ""
+// boolean => true/false
+//null => standlone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof age);
