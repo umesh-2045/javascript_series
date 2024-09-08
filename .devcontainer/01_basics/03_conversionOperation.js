b60bbf4a9bce9b8e1cc59852541604b360fecfad
umesh-2045 <umeshtejas2004@gmail.com>
2024-09-08 13:43:51 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,12 +15,34 @@ true = 1; false= 0
 "hitesh" = true
 */
 
-let isloggedin = 1
-let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+// let isloggedin = 1
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
 
-let somenumber = 33
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let somenumber = 33
+// let stringnumber = String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
+//  operations ///
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
