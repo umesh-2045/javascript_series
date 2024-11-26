e2b6f3f8518d4186a91a7acfe979e3093497a3e6
umesh-2045 <umeshtejas2004@gmail.com>
2024-11-26 15:26:29 +0000

    do ehile loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myarray = ["flash", "batman", "superman"]
+
+let arr = 0
+while (arr < myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
