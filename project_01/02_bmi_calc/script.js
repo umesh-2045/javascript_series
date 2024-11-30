65d9d49508c93015deb54a9ac3e41de7a5ff1fc1
Umesh Jagtap <umeshtejas2004@gmail.com>
2024-11-30 07:30:32 +0000

    bmi

---
+++ project_01/02_bmi_calc/script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = (document.querySelector('#results'))
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `please give a valid height${height}`
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `please give a valid weight ${weight}`
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+        results.innerHTML = `<span>${bmi}</span>`
+        if (bmi < 19) {
+            results.innerHTML = `<span> you are under weight${bmi}</span>`
+        }
+        if (bmi >= 19 || bmi <= 25) {
+            results.innerHTML = `<span> you are in normal range${bmi}</span>`
+        }
+        if (bmi > 25) {
+            results.innerHTML = `<span> you are Over weight${bmi}</span>`
+        }
+    }
+})
